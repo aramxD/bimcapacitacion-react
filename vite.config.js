dc999073fb56ca85130fcfdea42f3508bdda0983
julio santacruz <aram.jsh1@gmail.com>
2022-11-16 23:41:06 -0800

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: '/bimcapacitacion-react/',
-  base:'/',
+  base:'https://cursos.bimcapacitacion.com/',
   plugins: [react()]
 })
